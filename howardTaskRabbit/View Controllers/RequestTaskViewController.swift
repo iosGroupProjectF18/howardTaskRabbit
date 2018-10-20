8d8cc1a8e4e15073d0c3370a19af29f8fd3ce203
Whitney Griffith <whitney.griffith@bison.howard.edu>
2018-10-20 12:05:28 -0500

    Need to post task to database

--- howardTaskRabbit/View Controllers/RequestTaskViewController.swift
+++ howardTaskRabbit/View Controllers/RequestTaskViewController.swift
@@ -10,21 +10,26 @@ import UIKit
 
 class RequestTaskViewController: UIViewController {
 
+    @IBOutlet weak var taskInput: UITextField!
+    @IBOutlet weak var DoneBy: UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func submit(_ sender: Any) {
+        var taskObj = Task(task: taskInput.text as! String, date: DoneBy.date)
+        
+        //submit new task to list of tasks
+        
+        
+        
     }
-    */
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            let viewRequestController = segue.destination as! viewRequestController
+        }
+    
 }
